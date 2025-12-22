01f01cfb5be0489ee3824e37526be022d8b431c0
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2025-12-21 23:26:07 -0500

    design fix

--- script.js
+++ script.js
@@ -143,6 +143,6 @@ staggerCards(solutionCards);
 const serviceCards = document.querySelectorAll('.service-card');
 staggerCards(serviceCards);
 
-// Apply stagger to impact cards
-const impactCards = document.querySelectorAll('.impact-card');
-staggerCards(impactCards);
+// Apply stagger to impact panels
+const impactPanels = document.querySelectorAll('.impact-panel');
+staggerCards(impactPanels);
